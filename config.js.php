0e4c29e776d9550275b05204c941e07e958dda6e
Rocon <qiujiangkun@foxmail.com>
2018-09-28 18:36:35 +0800

    add config

---
+++ config.js.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: qiujaingkun
+ * Date: 2018/9/28
+ * Time: 18:26
+ */
+$config_filename = './config.js';
+$default_config = <<<TAG
+
+RMD_config = {
+	'enable': true,
+	'latex': "MathJax",
+	'info': 'default config'
+};
+TAG;
+if ( file_exists( $config_filename ) ) {
+	$config = $default_config;
+}else{
+	$f = fopen($config_filename, "r");
+	$config = fread($f, filesize($config_filename));
+}
+echo $config;
