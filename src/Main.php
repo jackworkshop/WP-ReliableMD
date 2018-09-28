c61c12c5bf52c9f883b4ff0e9ca4d85c30bcf429
jack960330 <jack9603301@163.com>
2018-09-28 21:22:06 +0800

    实现接口wp-json/WPReliableMD/config

--- src/Main.php
+++ src/Main.php
@@ -5,20 +5,30 @@
 namespace WPReliableMD;
 
 class Main {
+	protected $config_filename;
 	/* 构造函数 */
 	public function __construct() {
+		$this->config_filename = WPReliableMD_PATH.'\\config.json';
 		add_action( 'rest_api_init', array($this,'WPReliableMD_Api_Init'));
 	}
 	public function WPReliableMD_Api_Init() {
 		register_rest_route('WPReliableMD', 'config', [
 			'methods'   => 'GET',
-			'callback'  => 'WPReliableMD_Config_Api'
+			'callback'  => array($this,'WPReliableMD_Config_Api')
 		]);
 	}
 	public function WPReliableMD_Config_Api() {
-		return json_encode([
-			'enable' => true
-		]);
+		if ( file_exists( $this->config_filename ) ) {
+			$f = fopen($this->config_filename, "r");
+			$config = fread($f, filesize($this->config_filename));
+			return json_decode($config,TRUE);
+		} else {
+			return [
+				'enable' => true,
+				'latex' => "MathJax",
+				'info' => 'default config'
+			];
+		}
 	}
 }
 
