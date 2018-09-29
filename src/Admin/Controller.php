c1115c37b424f3a357e2a325b485c0a988766d2c
jack960330 <jack9603301@163.com>
2018-09-30 00:13:52 +0800

    修正目录和文件名

--- src/Admin/Controller.php
+++ src/Admin/Controller.php
@@ -16,14 +16,14 @@ class Controller {
 		//Requirejs库
 		wp_enqueue_script('Require', '//cdn.jsdelivr.net/npm/requirejs/require.min.js', array(), WPReliableMD_VER, true);
 		wp_enqueue_script('Bootstrap', '//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js', array(), WPReliableMD_VER, true);
-		wp_enqueue_script( 'ReliableMD', WPReliableMD_URL . '/js/WPReliableMD.js', array(), WPReliableMD_VER, true );
+		wp_enqueue_script( 'ReliableMD', WPReliableMD_URL . '/js/WPReliableMD_Admin.js', array(), WPReliableMD_VER, true );
 	}
 
 	public function enqueue_style() {
 		//Requirejs库
 		wp_enqueue_style('normalize', '//necolas.github.io/normalize.css/8.0.0/normalize.css', array(), WPReliableMD_VER, true);
 		wp_enqueue_style('Bootstrap', '//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css', array(), WPReliableMD_VER, true);
-		wp_enqueue_style( 'ReliableMD', WPReliableMD_URL . '/css/WPReliableMD.css', array(), WPReliableMD_VER, true );
+		wp_enqueue_style( 'ReliableMD', WPReliableMD_URL . '/css/WPReliableMD_Admin.css', array(), WPReliableMD_VER, true );
 	}
 }
 
