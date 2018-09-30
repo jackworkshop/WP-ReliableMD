fa4bfab0ea9da9a7048e6550b60149c7447ee6e4
Rocon <qiujiangkun@foxmail.com>
2018-09-30 19:33:39 +0800

    an editor

--- BackendEditor/mdeditor.js
+++ BackendEditor/mdeditor.js
@@ -64,24 +64,3 @@ function duplacateSingnal(text) {
 //todo
 }
 
-(function ($) {
-    $.fn.selectRange = function (start, end) {
-        if (end === undefined) {
-            end = start;
-        }
-        return this.each(function () {
-            if ('selectionStart' in this) {
-                this.selectionStart = start;
-                this.selectionEnd = end;
-            } else if (this.setSelectionRange) {
-                this.setSelectionRange(start, end);
-            } else if (this.createTextRange) {
-                var range = this.createTextRange();
-                range.collapse(true);
-                range.moveEnd('character', end);
-                range.moveStart('character', start);
-                range.select();
-            }
-        });
-    };
-})(jQuery);
