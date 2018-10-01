c641ffdd43f9a00d698e35c3da3639cd3622a929
jack960330 <jack9603301@163.com>
2018-10-02 01:51:04 +0800

    结合wp，但存在问题

--- src/Admin/Controller.php
+++ src/Admin/Controller.php
@@ -14,15 +14,30 @@ class Controller {
 
 	public function enqueue_scripts() {
 		//Requirejs库
-		wp_enqueue_script('Require', '//cdn.jsdelivr.net/npm/requirejs/require.min.js', array(), WPReliableMD_VER, true);
-		wp_enqueue_script('Bootstrap', '//cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js', array(), WPReliableMD_VER, true);
+		wp_enqueue_script('jquery');
+		wp_enqueue_script('markdown-it', WPReliableMD_URL.'/bower_components/markdown-it/dist/markdown-it.js', array(), WPReliableMD_VER, true);
+		wp_enqueue_script('to-mark', WPReliableMD_URL.'/bower_components/to-mark/dist/to-mark.js', array(), WPReliableMD_VER, true);
+		wp_enqueue_script('tui-code-snippet', WPReliableMD_URL.'/bower_components/tui-code-snippet/dist/tui-code-snippet.js', array(), WPReliableMD_VER, true);
+		wp_enqueue_script('tui-color-picker', WPReliableMD_URL.'/bower_components/tui-color-picker/dist/tui-color-picker.js', array(), WPReliableMD_VER, true);
+		wp_enqueue_script('codemirror', WPReliableMD_URL.'/bower_components/codemirror/lib/codemirror.js', array(), WPReliableMD_VER, true);
+		wp_enqueue_script('highlightjs', WPReliableMD_URL.'/bower_components/highlightjs/highlight.pack.js', array(), WPReliableMD_VER, true);
+		wp_enqueue_script('squire-rte', WPReliableMD_URL.'/bower_components/squire-rte/build/squire-raw.js', array(), WPReliableMD_VER, true);
+		wp_enqueue_script('plantuml-encoder', WPReliableMD_URL.'/bower_components/plantuml-encoder/dist/plantuml-encoder.js', array(), WPReliableMD_VER, true);
+		wp_enqueue_script('raphael', WPReliableMD_URL.'/bower_components/raphael/raphael.js', array(), WPReliableMD_VER, true);
+		wp_enqueue_script('tui-chart', WPReliableMD_URL.'/bower_components/tui-chart/dist/tui-chart.js', array(), WPReliableMD_VER, true);
+		wp_enqueue_script('tui-editor', WPReliableMD_URL.'/bower_components/tui-editor/dist/tui-editor-Editor-all.js', array(), WPReliableMD_VER, true);
 		wp_enqueue_script( 'ReliableMD', WPReliableMD_URL . '/js/WPReliableMD_Admin.js', array(), WPReliableMD_VER, true );
 	}
 
 	public function enqueue_style() {
 		//Requirejs库
 		wp_enqueue_style('normalize', '//necolas.github.io/normalize.css/8.0.0/normalize.css', array(), WPReliableMD_VER, true);
-		wp_enqueue_style('Bootstrap', '//cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css', array(), WPReliableMD_VER, true);
+		wp_enqueue_style('codemirror', WPReliableMD_URL.'/bower_components/codemirror/lib/codemirror.css', array(), WPReliableMD_VER, true);
+		wp_enqueue_style('github', WPReliableMD_URL.'/bower_components/highlightjs/styles/github.css', array(), WPReliableMD_VER, true);
+		wp_enqueue_style('tui-editor', WPReliableMD_URL.'/bower_components/tui-editor/dist/tui-editor.css', array(), WPReliableMD_VER, true);
+		wp_enqueue_style('tui-editor', WPReliableMD_URL.'/bower_components/tui-editor/dist/tui-editor-contents.css', array(), WPReliableMD_VER, true);
+		wp_enqueue_style('tui-color-picker', WPReliableMD_URL.'/bower_components/tui-color-picker/dist/tui-color-picker.css', array(), WPReliableMD_VER, true);
+		wp_enqueue_style('tui-chart', WPReliableMD_URL.'/bower_components/tui-chart/dist/tui-chart.css', array(), WPReliableMD_VER, true);
 		wp_enqueue_style( 'ReliableMD', WPReliableMD_URL . '/css/WPReliableMD_Admin.css', array(), WPReliableMD_VER, true );
 	}
 
