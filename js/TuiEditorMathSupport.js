b29682dbc04ad2f9ada90d2aa34a3fe2d5f9b89c
Rocon <qiujiangkun@foxmail.com>
2018-10-04 07:59:14 +0800

    Mathjax

--- js/TuiEditorMathSupport.js
+++ js/TuiEditorMathSupport.js
@@ -1,25 +1,53 @@
-(function(root, factory) {
+(function (root, factory) {
     if (typeof define === 'function' && define.amd) {
-        define(['tui-editor'], factory);
+        define(['tui-editor', 'markdown-it-mathjax', 'katex'], factory);
     } else if (typeof exports === 'object') {
         factory(require('tui-editor'));
     } else {
         factory(root['tui']['Editor']);
     }
-})(this, function(Editor) {
-    Editor.defineExtension('mathsupport', function() {
+})(this, function (Editor) {
+    // Editor.defineExtension('youtube', function () {
+    //     Editor.codeBlockManager.setReplacer('youtube', function (youtubeId) {
+    //         var wrapperId = 'yt' + Math.random().toString(36).substr(2, 10);
+    //         setTimeout(renderYoutube.bind(null, wrapperId, youtubeId), 0);
+    //
+    //         return '<div id="' + wrapperId + '"></div>';
+    //     });
+    // });
+    //
+    // function renderYoutube(wrapperId, youtubeId) {
+    //     var el = document.querySelector('#' + wrapperId);
+    //     el.innerHTML = '<iframe width="420" height="315" src="https://www.youtube.com/embed/' + youtubeId + '"></iframe>';
+    // }
+    window.edt = Editor;
+    Editor.defineExtension('mathsupport', function () {
+        console.log('enabled');
         var options = {
-            inlineOpen: '$',
-            inlineClose: '$',
-            blockOpen: '$$',
-            blockClose: '$$',
-            renderingOptions: {},
-            inlineRenderer: require('ascii2mathml')(this.rendererOptions),
-            blockRenderer: require('ascii2mathml')(Object.assign({ display: 'block' },
-                this.renderingOptions))
-        };
-        previewer.convertor.constructor.getMarkdownitHighlightRenderer()
-            .use(require('markdown-it-math'), options);
-    });
+                inlineOpen: '$',
+                inlineClose: '$',
+                blockOpen: '$$',
+                blockClose: '$$',
+                renderingOptions: {},
+                inlineRenderer: function (lt) {
+                    console.log(lt);
+                    var html = require('katex').renderToString(lt);
+                    console.log(html);
+                    return html;
+                },
+                blockRenderer: function (lt) {
+                    console.log(lt);
+                    var html = require('katex').renderToString(lt);
+                    console.log(html);
+                    return html;
+                }
 
+            }
+        ;
+        Editor.markdownitHighlight
+            .use(require('markdown-it-mathjax'));
+    })
 });
+
+
+
---
+++ js/require_paths.js
@@ -0,0 +1,31 @@
+function configure_requirejs() {
+    requirejs.config({
+        //By default load any module IDs from js/lib
+        baseUrl: '../bower_components/',
+        //except, if the module ID starts with "app",
+        //load it from the js/app directory. paths
+        //config is relative to the baseUrl, and
+        //never includes a ".js" extension since
+        //the paths config could be for a directory.
+        paths: {
+            'tui-editor': 'tui-editor/dist/tui-editor-Editor-all',
+            'jquery': 'jquery/dist/jquery',
+            'codemirror': 'codemirror/lib/codemirror',
+            'markdown-it': 'markdown-it/dist/markdown-it',
+            'to-mark': 'to-mark/dist/to-mark',
+            'tui-code-snippet': 'tui-code-snippet/dist/tui-code-snippet',
+            'tui-color-picker': 'tui-color-picker/dist/tui-color-picker',
+            'highlightjs': 'highlightjs/highlight.pack',
+            'squire-rte': 'squire-rte/build/squire-raw',
+            'plantuml-encoder': 'plantuml-encoder/dist/plantuml-encoder',
+            'tui-chart': 'tui-chart/dist/tui-chart',
+            'raphael': 'raphael/raphael',
+            'mathsupport': '../js/TuiEditorMathSupport',
+            'katex': 'katex/dist/katex',
+            'mathjax': 'mathjax/dist/mathjax',
+            'markdown-it-mathjax': '../js/markdown-it-mathjax/markdown-it-mathjax',
+        }
+    });
+}
+configure_requirejs();
+
