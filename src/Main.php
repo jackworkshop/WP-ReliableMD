24c6f1f614d691ca68a7c07556ff1cd27bff40e4
jack960330 <jack9603301@163.com>
2018-10-05 22:38:19 +0800

    优化代码

--- src/Main.php
+++ src/Main.php
@@ -9,23 +9,15 @@ use WPReliableMD\Environment\Controller as EnvironmentController;
 
 class Main {
 
-	private $env;
-
-	private $rest;
-
-	private $view;
-
-	private $admin;
-
 	public function __construct() {
 
-		$this->env = new EnvironmentController(); //初始化插件环境控制器
+		new EnvironmentController(); //初始化插件环境控制器
 
-		$this->rest = new RestController();  //初始化REST控制器
+		new RestController();  //初始化REST控制器
 
-		$this->view = new ViewController(); //初始化前端渲染控制器
+		new ViewController(); //初始化前端渲染控制器
 
-		$this->admin = new AdminController();
+		new AdminController();
 
 	}
 
