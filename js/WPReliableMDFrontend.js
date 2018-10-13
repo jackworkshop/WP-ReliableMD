0ebf51afe30d21375e7686f509788326562e17d1
qiujiangkun <qiujiangkun@foxmail.com>
2018-10-13 23:37:58 +0800

    cache support

--- js/WPReliableMDFrontend.js
+++ js/WPReliableMDFrontend.js
@@ -1,8 +1,35 @@
+requirejs(['jquery'], function($){
+	var hash = function(text){
+		var h = 0;
+		for(var i = 0; i < text.length; ++i){
+			h = h * 10007 + text[i].charCodeAt();
+			h %= 1e9 + 7;
 
-requirejs(['jquery', 'ReliableMD_render'], function ($, render) {
-    render.setCallback(function ($node) {
-        //return jQuery('markdown-block');
+		}
+		return "" + h;
+	};
+	var callback = function ($node) {
         return $node.parent().attr('class') === 'markdown-block' ? $node.parent() : $node;
-    });
+    };
+	var cached = function(text)
+	{
+		return window.localStorage.getItem(hash(text));
+	};
+	var cnt = 0;
+	
+	$('.markdown').each(function () {
+		var text = $(this).val() || $(this).html();
+		var ca = cached(text);
+		if(ca === null)
+			++cnt;
+		else{
+			console.log("used cache", hash(text));
+			callback($(this)).html(ca);
+		}
+	});
+	if(cnt > 0) {
+		requirejs(['ReliableMD_render'], function (render) {
+    		render.setCallback(callback);
+		});
+	}
 });
-
