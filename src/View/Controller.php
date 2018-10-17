4d86a76e01eaa6c6c7b4ec8df0c86befe5626687
qiujiangkun <qiujiangkun@foxmail.com>
2018-10-17 22:16:28 +0800

    chinese support

--- src/View/Controller.php
+++ src/View/Controller.php
@@ -3,7 +3,47 @@
 namespace WPReliableMD\View;
 
 use WPReliableMD\View\Parser as Parser;
+	/*
+	Utf-8、gb2312都支持的汉字截取函数
+	cut_str(字符串, 截取长度, 开始长度, 编码);
+	编码默认为 utf-8
+	*/
 
+	function cut_str($string, $start, $sublen, $code = 'UTF-8')
+	{
+		if($code == 'UTF-8')
+		{
+			$pa = "/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/";
+			preg_match_all($pa, $string, $t_string);
+
+			if(count($t_string[0]) - $start > $sublen) return join('', array_slice($t_string[0], $start, $sublen));
+			return join('', array_slice($t_string[0], $start, $sublen));
+		}
+		else
+		{
+			$start = $start*2;
+			$sublen = $sublen*2;
+			$strlen = strlen($string);
+			$tmpstr = '';
+
+			for($i=0; $i< $strlen; $i++)
+			{
+				if($i>=$start && $i< ($start+$sublen))
+				{
+					if(ord(substr($string, $i, 1))>129)
+					{
+						$tmpstr.= substr($string, $i, 2);
+					}
+					else
+					{
+						$tmpstr.= substr($string, $i, 1);
+					}
+				}
+				if(ord(substr($string, $i, 1))>129) $i++;
+			}
+			return $tmpstr;
+		}
+	}
 class Controller {
 
 	public function __construct() {
@@ -64,7 +104,7 @@ class Controller {
 			}
 		}
 
-		return do_shortcode(substr( $post_excerpt, 0, apply_filters('excerpt_length',50) ));
+		return do_shortcode(cut_str( $post_excerpt, 0, apply_filters('excerpt_length',50) ));
 	}
 
 	public function enqueue_scripts() {
