85c40d79940665ae87caf56e5f694498d97b27ea
jack960330 <jack9603301@163.com>
2018-10-18 15:19:20 +0800

    修正bug
    修改中文支持的代码形式

--- src/View/Controller.php
+++ src/View/Controller.php
@@ -15,33 +15,57 @@ class Controller {
 		//markdown解析
 		add_filter( 'the_content', array( $this, 'WPReliableMD_the_Content' ) );
 		add_filter( 'the_excerpt', array( $this, 'the_excerpt' ) );
-		add_filter('markdown_backend_rendered',array($this,'WPReliableMD_BackendRendered'),1,3);
+		add_filter('markdown_backend_rendered',array($this,'WPReliableMD_BackendRendered'),1,4);
 		add_filter('markdown_text',array($this,'WPReliableMD_MarkdownText_Transference'),1,2);
 		add_filter('markdown_shortcode_text',array($this,'WPReliableMD_MarkdownShortcodeText_AntiTransfer'),1);
 		add_filter('widget_text', 'do_shortcode');
+		add_filter('markdown_the_excerpt',array($this,'cut_str'),1,2);
+		add_filter('markdown_excerpt_cut_str',array($this,'cut_str_utf8'),1,3);
+		add_filter('markdown_excerpt_cut_str',array($this,'cut_str_gb2312'),1,3);
 
 		add_shortcode('markdown',array($this,'WPReliableMD_Shortcode_Markdown'));
 
 	}
 
-	/*
-	Utf-8、gb2312都支持的汉字截取函数
-	cut_str(字符串, 截取长度, 开始长度, 编码);
-	编码默认为 utf-8
-	*/
-
-	function cut_str($string, $start, $sublen, $code = 'UTF-8')
+	public function cut_str($string,$is_auto_get_excerpt)
 	{
-		if($code == 'UTF-8')
-		{
+		$code = get_bloginfo('charset');
+		$sublen = apply_filters('excerpt_length',50);
+		if($is_auto_get_excerpt) {
+			/*
+			 * filter  : markdown_excerpt_cut_str($string,$sublen,$code)
+			 * comment : Extended coding support function covering markdown automatic interception summary mechanism.
+			 * params  :
+			 *   - $string : Pre processing string.
+			 *   - $sublen : Processing string length.
+			 *   - $code : Encoding of processing data.
+			 */
+			$string = apply_filters('markdown_excerpt_cut_str',$string,$sublen,$code);
+		}
+		return $string;
+	}
+
+	public function cut_str_utf8($string,$sublen,$code) {
+		$start = 0;
+		if($code == "UTF-8") {
+			//UTF-8处理
 			$pa = "/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/";
 			preg_match_all($pa, $string, $t_string);
 
-			if(count($t_string[0]) - $start > $sublen) return join('', array_slice($t_string[0], $start, $sublen));
-			return join('', array_slice($t_string[0], $start, $sublen));
+			if(count($t_string[0]) - $start > $sublen) {
+				$string = join('', array_slice($t_string[0], $start, $sublen));
+			} else {
+				$string = join('', array_slice($t_string[0], $start, $sublen));
+			}
 		}
-		else
-		{
+		
+		return $string;
+	}
+
+	public function cut_str_gb2312($string,$sublen,$code) {
+		$start = 0;
+		if($code == "GB2312") {
+			//GB2312
 			$start = $start*2;
 			$sublen = $sublen*2;
 			$strlen = strlen($string);
@@ -60,10 +84,13 @@ class Controller {
 						$tmpstr.= substr($string, $i, 1);
 					}
 				}
-				if(ord(substr($string, $i, 1))>129) $i++;
+				if(ord(substr($string, $i, 1))>129) {
+					$i++;
+				}
 			}
-			return $tmpstr;
+			$string = $tmpstr;
 		}
+		return $string;
 	}
 
 	function the_excerpt( $post_excerpt ) {
@@ -81,8 +108,9 @@ class Controller {
 			 *   - $backend_rendered : The output of a summary or the back end of an article.
 			 *   - $content : Subject before treatment
 			 *   - $excerpt_bool : If it is an article, it is false, if it is a summary, then it is true.
+			 *   - $is_shortcode_tag : If the short tag parser is parsed, it is true, otherwise it is false.
 			 */
-			$post_excerpt = apply_filters('markdown_backend_rendered',$post_excerpt,$post->post_content,true);
+			$post_excerpt = apply_filters('markdown_backend_rendered',$post_excerpt,$post->post_content,true,false);
 			if ( preg_match( '#<p>((\w|\d|[^x00-xff]).+?)</p>#', $post_excerpt, $mc ) ) {
 				$post_excerpt = $mc[1];
 				/*
@@ -106,7 +134,7 @@ class Controller {
 			}
 		}
 
-		return do_shortcode($this->cut_str( $post_excerpt, 0, apply_filters('excerpt_length',50) ));
+		return do_shortcode($post_excerpt);
 	}
 
 	public function enqueue_scripts() {
@@ -146,7 +174,7 @@ class Controller {
 		 *   - $markdown : Markdown text before input processing.
 		 */
 		$content = apply_filters('markdown_shortcode_text',$content);
-		return do_shortcode($this->WPReliableMD_Content($content));  //解析，执行
+		return do_shortcode($this->WPReliableMD_Content($content,true));  //解析，执行
 	}
 
 	public function WPReliableMD_MarkdownShortcodeText_AntiTransfer($markdown)  {
@@ -170,7 +198,7 @@ class Controller {
 		return $markdown;
 	}
 
-	public function WPReliableMD_Content( $content ) {
+	public function WPReliableMD_Content( $content,$is_shortcode_tag = false ) {
 
 		$backend_rendered = null;
 
@@ -193,8 +221,9 @@ class Controller {
 		*   - $backend_rendered : The output of a summary or the back end of an article.
 		*   - $content : Subject before treatment
 		*   - $excerpt_bool : If it is an article, it is false, if it is a summary, then it is true.
+		*   - $is_shortcode_tag : If the short tag parser is parsed, it is true, otherwise it is false.
 		*/
-		$backend_rendered = apply_filters('markdown_backend_rendered',$backend_rendered,$backend_rendered_text,false);  //可由用户覆盖解析效果
+		$backend_rendered = apply_filters('markdown_backend_rendered',$backend_rendered,$backend_rendered_text,false,$is_shortcode_tag);  //可由用户覆盖解析效果
 
 		/*
 		* filter  : markdown_text($markdown)
@@ -236,25 +265,31 @@ class Controller {
 		return $markdown;
 	}
 
-	public function WPReliableMD_BackendRendered($backend_rendered,$content,$excerpt_bool) {
-		$post_id = get_the_ID();
-		if($excerpt_bool) {
-			//如果是摘要缓存
-			$backend_rendered = wp_cache_get($post_id,'markdown_backend_rendered:excerpt');
-			if($backend_rendered === false) {
-				$parser = new Parser();
-				$backend_rendered = $parser->makeHtml( $content );
-				wp_cache_set($post_id,$backend_rendered,'markdown_backend_rendered:excerpt');
+	public function WPReliableMD_BackendRendered($backend_rendered,$content,$excerpt_bool,$is_shortcode_tag) {
+		if(!$is_shortcode_tag) {
+			$post_id = get_the_ID();
+			if($excerpt_bool) {
+				//如果是摘要缓存
+				$backend_rendered = wp_cache_get($post_id,'markdown_backend_rendered:excerpt');
+				if($backend_rendered === false) {
+					$parser = new Parser();
+					$backend_rendered = $parser->makeHtml( $content );
+					wp_cache_set($post_id,$backend_rendered,'markdown_backend_rendered:excerpt');
+				}
+			} else {
+				//如果是文章缓存
+				$backend_rendered = wp_cache_get($post_id,'markdown_backend_rendered');
+				if($backend_rendered === false) {
+					$parser = new Parser();
+					$backend_rendered = $parser->makeHtml( $content );
+					wp_cache_set($post_id,$backend_rendered,'markdown_backend_rendered');
+				}
 			}
 		} else {
-			//如果是文章缓存
-			$backend_rendered = wp_cache_get($post_id,'markdown_backend_rendered');
-			if($backend_rendered === false) {
-				$parser = new Parser();
-				$backend_rendered = $parser->makeHtml( $content );
-				wp_cache_set($post_id,$backend_rendered,'markdown_backend_rendered');
-			}
+			$parser = new Parser();
+			$backend_rendered = $parser->makeHtml( $content );
 		}
+		
 		
 		return $backend_rendered;
 	}
