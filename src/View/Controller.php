aba4071dd0c9365c5535c27d82acb373810cf99c
jack960330 <jack9603301@163.com>
2018-10-18 15:20:51 +0800

    修正

--- src/View/Controller.php
+++ src/View/Controller.php
@@ -52,11 +52,7 @@ class Controller {
 			$pa = "/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|\xe0[\xa0-\xbf][\x80-\xbf]|[\xe1-\xef][\x80-\xbf][\x80-\xbf]|\xf0[\x90-\xbf][\x80-\xbf][\x80-\xbf]|[\xf1-\xf7][\x80-\xbf][\x80-\xbf][\x80-\xbf]/";
 			preg_match_all($pa, $string, $t_string);
 
-			if(count($t_string[0]) - $start > $sublen) {
-				$string = join('', array_slice($t_string[0], $start, $sublen));
-			} else {
-				$string = join('', array_slice($t_string[0], $start, $sublen));
-			}
+			$string = join('', array_slice($t_string[0], $start, $sublen));
 		}
 		
 		return $string;
