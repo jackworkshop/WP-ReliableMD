40f4a35c8085a5b38e45b571935a5cb143b0bdd6
qiujiangkun <qiujiangkun@foxmail.com>
2018-10-29 13:24:24 +0800

    fix a bug

--- src/View/Controller.php
+++ src/View/Controller.php
@@ -233,7 +233,7 @@ class Controller {
 
 
 		$new_content      = "<div class='markdown-block'>";
-		$new_content      .= "<pre class='markdown' style='display:none;'>{$content}</pre>";
+		$new_content      .= "<script class='markdown' type="text/markdown">{$content}</script>";
 		$new_content      .= "<div class='markdown-backend-rendered'>{$backend_rendered}</div>";
 		$new_content      .= "</div>";
 		$content          = $new_content;
