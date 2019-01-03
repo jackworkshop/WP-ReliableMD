7c578f4bed44821296bfe5c59cbc8d3eeb861001
jack960330 <jack9603301@163.com>
2019-01-03 15:29:16 +0800

    修改文件

--- js/WPReliableMDFrontend.js
+++ js/WPReliableMDFrontend.js
@@ -1,19 +1,4 @@
 requirejs(['jquery'], function($){
-	var $_GET = (function () {
-        var url = window.document.location.href.toString();
-        var u = url.split("?");
-        if (typeof(u[1]) === "string") {
-            u = u[1].split("&");
-            var get = {};
-            for (var i in u) {
-                var j = u[i].split("=");
-                get[j[0]] = j[1];
-            }
-            return get;
-        } else {
-            return {};
-        }
-    })();
 	var hash = function(text){
 		var h = 0;
 		for(var i = 0; i < text.length; ++i){
